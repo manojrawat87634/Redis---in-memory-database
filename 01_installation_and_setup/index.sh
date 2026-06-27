949dba6b24dbbf0b35746a7ae5ed025e046e0354
manojrawat87634 <manojrawat87634@gmail.com>
2026-06-27 10:16:31 +0530

    how to install redis

---
+++ 01_installation_and_setup/index.sh
@@ -0,0 +1,11 @@
+#update all packages
+sudo apt update
+
+#upgrade all packages
+sudo apt upgrade
+
+# Installation Command
+sudo apt install redis-server
+
+# Verify installation
+redis-cli --version
